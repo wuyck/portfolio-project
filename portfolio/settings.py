36461f09d490a9a512ddbe31c8715186c50a1115
Hans van Wuijckhuijse <hansvanwuyckhuyse@hotmail.com>
2021-01-01 17:34:43 +0000

    Finished the website

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -124,8 +124,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATICFILES_DIR = [
+    BASE_DIR / 'static'
+]
+STATIC_ROOT = BASE_DIR / 'static'
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
-
 MEDIA_URL = '/media/'
